194c6bd0b6335becea79cc5fb3950a8120c7ee0d
ShubhamBailwal <147264356+ShubhamBailwal@users.noreply.github.com>
2025-07-03 22:54:01 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/shubham-portfolio",
+  base: '/portfolio-website/', // ← this is crucial
 })
